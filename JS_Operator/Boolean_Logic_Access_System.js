755dd48df7653877968b84d62c56bf725c832d63
Rishabh Jaiswal <rishabhjaiswal16092004@gmail.com>
2025-11-17 22:11:19 +0530

    Delete JS_Operator directory

--- JS_Operator/Boolean_Logic_Access_System.js
+++
@@ -1,8 +0,0 @@
-let isLocked = true;
-let isWindowClosed = true;
-let isAlarmOn = true;
-let isOwnerInside = false;
-
-let isSecure = isAlarmOn && isLocked && isOwnerInside && isWindowClosed;
-
-console.log(isSecure ? "Secure" : "Unsafe");
