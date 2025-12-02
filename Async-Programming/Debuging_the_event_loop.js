f29dfd0c3ac32014895abac3726bd37278608760
Rishabh Jaiswal <rishabhjaiswal16092004@gmail.com>
2025-12-02 12:06:39 +0530

    Delete Async-Programming directory

--- Async-Programming/Debuging_the_event_loop.js
+++
@@ -1,8 +0,0 @@
-console.log("Script start");
-
-setTimeout(() => console.log("Timeout callback"), 0); // macrotask
-
-Promise.resolve()
-       .then(() => console.log("Promise callback"));   // microtask
-
-console.log("Script end");
